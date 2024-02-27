dacb8a7615fbf549e3098d4c201e2a63655c3e00
Pavel Zehnula <pavel.zehnula@olc.cz>
2024-02-27 14:56:31 +0100

    tasks

---
+++ 1regex/task1/email.py
@@ -0,0 +1,7 @@
+def validate_email(email):
+    """
+    This function checks if the given email address is valid or not.
+    :param email: str
+    :return: bool
+    """
+    pass
---
+++ 1regex/task2/password.py
@@ -0,0 +1,3 @@
+# Přijímá hesla o délce 8-16 znaků, obsahující alespoň jedno velké písmeno, jedno číslo a jeden speciální znak.
+def validate_password(password):
+    pass
