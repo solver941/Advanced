66fe9a3ef29c4e85013ff4158bbf093dd5f9e98c
Marrek Nožka <nozka@spseol.cz>
2024-04-03 12:44:51 +0200

    fix docstring

--- 1regex/task1/email.py
+++ 1regex/task1/email.py
@@ -1,7 +1,10 @@
-def validate_email(email):
+def validate_email(email:str) -> bool:
     """
     This function checks if the given email address is valid or not.
-    :param email: str
-    :return: bool
+
+    :param email: email address
+    :type email: str
+    :return: boolean value of valitation
+    :rtype: bool
     """
     pass
