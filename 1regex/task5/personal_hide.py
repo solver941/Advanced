3d1b8f4846d7873b658a779cf961cb20b7f05a33
solver941 <marek.heckelmoser@seznam.cz>
2024-04-25 19:51:05 +0200

    finished

--- 1regex/task5/personal_hide.py
+++ 1regex/task5/personal_hide.py
@@ -2,20 +2,24 @@ import re
 
 
 def phone_hide(persons: list[str]) -> list[str]:
-    #result = [re.sub(r"\d{3}-\d{3}-\d{3}", "***-***-"+person[:-4], person) for person in persons]
+    #structure = r"*: [0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9]"
     result = [re.sub(r"\d{3}-", "***-", person) for person in persons]
     return result
 
 
 def email_hide(persons: list[str]) -> list[str]:
-    for i in persons:
-        username, domain = i.split('@')
-        hidden_username = username[0] + '*' * (len(username) - 2) + username[-1]
-        hidden_domain = domain[0] + '*' * (len(domain) - 2) + domain[-1]
-
-
-
-    return f"{hidden_username}@{hidden_domain}"
+    return [re.sub(r": ([a-z])[a-z.]+([a-z])@[a-z]+([a-z])", r": \1***\2@***\3", person) for person in persons]
+    # result = []
+    # pattern = re.compile(r'([A-Z][a-z]): (?P[a-z])@(?P[a-z])\.(?P[a-z]+)')
+    # for i in persons:
+    #     match = re.match(pattern, i)
+    #     if match:
+    #         modified_email = match.group('name') + '***' + match.group('first')[0] + '@' + match.group(
+    #             'domain_name') + '.' + match.group('domain')
+    #         result.append(modified_email)
+    #     else:
+    #         result.append(i)  # If pattern doesn't match, keep original email
+    # return result
 
 
 
